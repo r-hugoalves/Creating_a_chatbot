f99e642721459c197395d826fa8323e1270db88c
Hugo R Alves <r.hugoalves66@gmail.com>
2023-01-16 23:29:53 -0300

    Implementação básica
    Foi feita a primeira implementação do bot, apenas para testar se todos os recursos (imports e o Flask) estavam funcionando corretamente.
    
    Ainda será implementando e corrigido:
    HTML
    CSS
    Base de dados do chatterbot

--- main.py
+++ main.py
@@ -1,8 +1,41 @@
-from flask import Flask
+from flask import Flask, request, render_template
+from chatterbot import ChatBot
+from chatterbot.trainers import ListTrainer
 
 #inicialização básica do Flask
 
 app = Flask(__name__)
+
+# criação do bot / nome do bot / armazenamento no banco de dados
+suporte_bot = ChatBot("Suporte do Hugo", storage_adapter="chatterbot.storage.SQLStorageAdapter")
+
+trainer = ListTrainer(suporte_bot)
+
+#O nosso chatbot vai aprender por meio de uma lista, com a seguinte estrutura:
+# lista = [
+#    "mensagem do usuário",
+#    "chatbot responde",
+#    "mensagem do usuário",
+#    "chatbot responde",
+#     ...
+#]
+
+trainer.train([
+  "Olá", 
+  "Olá, tudo bem? Como posso te ajudar?",
+  "Oi", 
+  "Oi, tudo bem? Como posso te ajudar?"
+])
+
+
+@app.route("/")
+def home():
+  return render_template("index.html")
+
+@app.route("/get")
+def get_bot_response():
+  userText = request.args.get("msg")
+  return str(suporte_bot.get_response(userText))
 
 @app.route("/hello") #definindo a rota (endereço)
 def hello():
